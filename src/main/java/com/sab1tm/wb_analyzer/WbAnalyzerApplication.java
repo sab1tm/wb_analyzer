747cc3623cff9d203ec78b1edca9d139ed877936
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2022-10-17 17:59:13 +0600

    feat: upload new project

---
+++ src/main/java/com/sab1tm/wb_analyzer/WbAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.sab1tm.wb_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sab1tm/wb_analyzer/WbAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sab1tm.wb_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
